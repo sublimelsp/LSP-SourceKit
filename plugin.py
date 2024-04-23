55fd5f23c8d414ad860633b887dc76486194550a
Yaroslav Yashin <yaroslav.yashin@gmail.com>
2024-04-23 14:57:04 +0200

    Redundant imports removed

--- plugin.py
+++ plugin.py
@@ -1,9 +1,6 @@
 from LSP.plugin import AbstractPlugin
-from LSP.plugin import ClientConfig
 from LSP.plugin import register_plugin
 from LSP.plugin import unregister_plugin
-from LSP.plugin import WorkspaceFolder
-import sublime
 
 # TODO: Once on Python 3.8, use the built-in typing module
 from LSP.plugin.core.typing import List
