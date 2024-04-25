e3a649752be07511fb0bd72fdd94e3e0d3fb50d2
Rafał Chłodnicki <rchl2k@gmail.com>
2024-04-25 20:25:24 +0200

    minor style and comment updates (#8)

--- plugin.py
+++ plugin.py
@@ -2,10 +2,12 @@ from LSP.plugin import AbstractPlugin
 from LSP.plugin import register_plugin
 from LSP.plugin import unregister_plugin
 
+
 class SourceKit(AbstractPlugin):
     @classmethod
     def name(cls) -> str:
         return cls.__name__
+
 
 def plugin_loaded() -> None:
     register_plugin(SourceKit)
